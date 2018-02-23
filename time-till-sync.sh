d150fd94e249e89c479e3cb87d13caaf36688a77
Michal Takac <takacmichal92@gmail.com>
2018-02-23 23:20:45 +0100

    in hours

--- time-till-sync.sh
+++ time-till-sync.sh
@@ -4,4 +4,5 @@ hash=`bitcoin-cli getblockhash ${cnt}`
 timeline=`bitcoin-cli getblock $hash | grep '"time"'`
 ltrimtime=${timeline#*time\" : }
 newest=${ltrimtime%%,*}
-echo $((`date +%s`-$newest))
+# echo $((`date +%s`-$newest)) # in miliseconds
+echo $(((`date +%s`-$newest)/3600)) # in hours
